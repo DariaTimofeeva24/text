42a867bf6a9d2ea13e1931cf81793f0278580463
Daria Kuzina <d.k.24@yandex.ru>
2022-03-08 17:04:44 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:text/switches_demo_screen.dart';
+import 'package:text/text_demo_screen.dart';
+
+import 'app_bar_screen.dart';
+import 'navigation_drawer_screen.dart';
+
+
+void main() {
+  runApp(const SwitchesDemo());
+}
